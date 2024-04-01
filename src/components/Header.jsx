a86a8a764fc289cda24723d4a407535bd8e133c7
SoftLife0 <onikosiadewale18@gmail.com>
2024-04-01 21:27:55 +0000

    just completed the Hero Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import logo from '../assets/img/logo192.png'
+import logo from '../assets/img/fav.png'
 
 const Header = () => {
   return (
     <header id="header" className="fixed-top">
       <div className="container d-flex align-items-center justify-content-between">
         <h1 className="logo">
-          <a href="#">
-            <img src={logo} width="150%" alt="" srcSet="" />
+          <a href="#" style={{display:'flex', justifyContent:'space-around', alignItems:'center'}}>
+            <img src={logo} width="60" alt="" srcSet="" />
+            <h6 style={{paddingTop:'5px'}}><b>Centrel University <br/>Day of Giving</b></h6>
           </a>
         </h1>
         <nav id="navbar" className="navbar">
           <ul>
             <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
-            <li><a className="nav-link scrollto" href="#about">Our Story</a></li>
+            <li><a className="nav-link scrollto" href="#about">About Us</a></li>
             <li className="dropdown">
               <a href="#"><span>Projects</span> <i className="bi bi-chevron-down"></i></a>
               <ul>
@@ -22,9 +23,7 @@ const Header = () => {
               </ul>
             </li>
             <li><a className="nav-link scrollto" href="#services">Statements</a></li>
-            <li><a className="nav-link scrollto" href="#portfolio">Gallery</a></li>
-            <li><a className="nav-link scrollto" href="#team">Team</a></li>
-            <li><a className="nav-link scrollto" href="#footer">Contact</a></li>
+            <li><a className="nav-link scrollto" href="#team">Testimonial</a></li>
             <li>
               <a className="getstarted scrollto" style={{ background: '#ff4a17', border: '#ff4a17' }} href="https://seaportcommunitychurch.churchcenter.com/calendar/event/137433448">
                 Donate Now
